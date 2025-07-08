1984512f4734d12ffebb3c69c53a62978745a374
Alekseypavlov14 <aleshapavlov9@gmail.com>
2025-07-08 10:36:49 +0300

    added catalog dropdown

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-// Header dropdowns
+// ========== Header dropdowns ==========
+// toggling
 const headerCustomersDropdownTrigger = document.getElementById('header-customers-dropdown-trigger')
 const headerCustomersDropdown = document.getElementById('header-customers-dropdown')
 
-const headerCustomersDropdownOpenedCSSClass = 'header-customers-dropdown--opened'
+const headerCatalogDropdownTrigger = document.getElementById('header-catalog-dropdown-trigger')
+const headerCatalogDropdown = document.getElementById('header-catalog-dropdown')
+
+const headerDropdownOpenedCSSClass = 'header-dropdown--opened'
 
 headerCustomersDropdownTrigger.addEventListener('click', () => {
-  headerCustomersDropdown.classList.toggle(headerCustomersDropdownOpenedCSSClass)
+  headerCustomersDropdown.classList.toggle(headerDropdownOpenedCSSClass)
+})
+headerCatalogDropdownTrigger.addEventListener('click', () => {
+  headerCatalogDropdown.classList.toggle(headerDropdownOpenedCSSClass)
+})
+
+// load icons
+const headerCatalogLinkIcons = Array.from(document.querySelectorAll('.header-catalog-dropdown-menu__link-icon'))
+const headerCatalogLinksDataPathAttribute = 'data-path'
+
+headerCatalogLinkIcons.forEach(icon => {
+  icon.style.setProperty('--path', icon.getAttribute(headerCatalogLinksDataPathAttribute))
 })
