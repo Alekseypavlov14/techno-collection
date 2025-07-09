c001a15f048af44d16acb6996f452808a80afc8d
Alekseypavlov14 <aleshapavlov9@gmail.com>
2025-07-09 10:48:39 +0300

    added home-articles block

--- app.js
+++ app.js
@@ -16,3 +16,24 @@ headerCatalogDropdownTrigger.addEventListener('click', () => {
   headerCustomersDropdown.classList.remove(headerDropdownOpenedCSSClass)
   headerCatalogDropdown.classList.toggle(headerDropdownOpenedCSSClass)
 })
+
+// ========== Tabs ==========
+const tabs = Array.from(document.querySelectorAll('[data-tabs]'))
+
+const tabsLinkActiveClass = 'tabs__link--active'
+const tabsPagesActiveClass = 'tabs__page--active'
+
+tabs.forEach(tab => {
+  const links = Array.from(document.querySelectorAll('[data-tabs-link]'))
+  const pages = Array.from(document.querySelectorAll('[data-tabs-page]'))
+
+  links.forEach((link, index) => {
+    link.addEventListener('click', () => {
+      links.forEach(link => link.classList.remove(tabsLinkActiveClass))
+      link.classList.add(tabsLinkActiveClass)
+      
+      pages.forEach(page => page.classList.remove(tabsPagesActiveClass))
+      pages[index].classList.add(tabsPagesActiveClass)
+    })
+  })
+})
