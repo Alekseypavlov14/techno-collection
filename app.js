f9f3a30715118872c11cc4d15c9b15a3cfca3d00
Alekseypavlov14 <aleshapavlov9@gmail.com>
2025-07-10 13:46:38 +0300

    added range

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+// ========== Constants ==========
+const PERCENTS = 100
+
+const NORMALIZED_MIN = 0
+const NORMALIZED_MAX = 1
+
 // ========== Header dropdowns ==========
 // toggling
 const headerCustomersDropdownTrigger = document.getElementById('header-customers-dropdown-trigger')
@@ -37,3 +43,113 @@ tabs.forEach(tab => {
     })
   })
 })
+
+// ========== Range ==========
+const ranges = Array.from(document.querySelectorAll('[data-range]'))
+
+ranges.forEach(range => {
+  const track = range.querySelector('[data-range-track]')
+  const pointMin = range.querySelector('[data-range-point-min]')
+  const pointMax = range.querySelector('[data-range-point-max]')
+
+  const labelMin = range.querySelector('[data-range-label-min]')
+  const labelMax = range.querySelector('[data-range-label-max]')
+
+  // check required elements
+  if (!track || !pointMin || !pointMax) return 
+
+  // store range boundaries
+  const rangeMin = Number(range.getAttribute('data-range-value-min') || NORMALIZED_MIN)
+  const rangeMax = Number(range.getAttribute('data-range-value-max') || NORMALIZED_MAX)
+
+  // store values
+  let valueMin = NORMALIZED_MIN
+  let valueMax = NORMALIZED_MAX
+
+  // init control
+  handleUpdateMin(valueMin)
+  handleUpdateMax(valueMax)
+
+  let isPointMinMoving = false
+  let isPointMaxMoving = false
+
+  // handle moving initiation
+  pointMin.addEventListener('mousedown', () => {
+    isPointMinMoving = true
+  })
+  pointMax.addEventListener('mousedown', () => {
+    isPointMaxMoving = true
+  })
+
+  // handle value changing 
+  document.addEventListener('mousemove', (e) => {
+    if (!isPointMinMoving && !isPointMaxMoving) return
+
+    const trackRect = track.getBoundingClientRect()
+    
+    const mouseX = e.clientX
+    const trackX = trackRect.x
+
+    const dx = mouseX - trackX
+    const trackWidth = trackRect.width
+
+    const newValue = clamp(NORMALIZED_MIN, dx / trackWidth, NORMALIZED_MAX)
+    
+    if (isPointMinMoving) handleUpdateMin(newValue)
+    if (isPointMaxMoving) handleUpdateMax(newValue)
+  })
+
+  // handle moving stop
+  window.addEventListener('mouseup', () => {
+    isPointMinMoving = false
+    isPointMaxMoving = false
+  })
+  window.addEventListener('mouseleave', () => {
+    isPointMinMoving = false
+    isPointMaxMoving = false
+  })
+  
+  // handle updates
+  function handleUpdateMin(value) {
+    // prevent min and max swap
+    if (value > valueMax) value = valueMax
+
+    // update state
+    valueMin = value
+
+    // update styles
+    range.style.setProperty('--value-min', `${value * PERCENTS}%`)   
+
+    // update hints
+    if (labelMin) labelMin.innerHTML = formatHintValue(computeAbsoluteValue(value))
+  }
+  function handleUpdateMax(value) {
+    // prevent min and max swap
+    if (value < valueMin) value = valueMin
+
+    // update state
+    valueMax = value
+
+    // update styles
+    range.style.setProperty('--value-max', `${value * PERCENTS}%`)
+
+    // update hints
+    if (labelMax) labelMax.innerHTML = formatHintValue(computeAbsoluteValue(value))
+  }
+
+  // utils
+  function computeAbsoluteValue(value) {
+    const amplitude = rangeMax - rangeMin
+    return Math.round(amplitude * value + rangeMin)
+  }
+  function formatHintValue(absoluteValue) {
+    return absoluteValue.toLocaleString('ru-Ru')
+  }
+})
+
+// ========== Utils ==========
+function clamp(min, value, max) {
+  if (value < min) return min
+  if (value > max) return max
+  return value
+}
